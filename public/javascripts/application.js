5c3416f3a5d16e532f5eaae0670dc9614b6c90e2
Ben Klang <bklang@mojolingo.com>
2011-04-20 09:24:47 -0400

    Fix recent commits display

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 
   $("a.recent_commits").click(function () {
-    $(this).siblings(".more_data").toggle(500);
+    $(this).parent().siblings(".more_data").toggle(500);
     $(this).text($(this).text() == "hide..." ? "recent commits..." : "hide...");
   });
 
