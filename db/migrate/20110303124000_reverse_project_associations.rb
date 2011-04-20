02f7f17b31a7308445cc699e4acc1ae433a030f1
Ben Klang <bklang@mojolingo.com>
2011-04-20 09:59:41 -0400

    Map existing repositories to projects

--- db/migrate/20110303124000_reverse_project_associations.rb
+++ db/migrate/20110303124000_reverse_project_associations.rb
@@ -4,6 +4,13 @@ class ReverseProjectAssociations < ActiveRecord::Migration
     remove_column :projects, :rubygem_id
     add_column :rubygems, :project_id, :integer
     add_column :repositories, :project_id, :integer
+
+    # Map existing repositories to new projects
+    Repository.all.each do |r|
+      p = r.create_project :created_at => r.created_at, :updated_at => r.updated_at
+      r.project = p
+      r.save
+    end
   end
 
   def self.down
