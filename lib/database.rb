3cefb31920891e536d81e2c50ada067e8172f66d
Jamey Owens <jowens@mojolingo.com>
2013-04-06 01:47:59 -0400

    remove comments from database.rb

--- lib/database.rb
+++ lib/database.rb
@@ -1,29 +1,3 @@
 set :environment, :development
 set :database,    'sqlite::memory'
 
-=begin
-puts "The foo table doesn't exist!" if !database.table_exists?('foos')
-
-migration "create teh foos table" do
-  database.create_table :foos do
-    primary_key :id
-    text        :bar
-    integer     :baz, :default => 42
-    timestamp   :bizzle, :null => false
-
-  index :baz, :unique => true
-  end
-end
-
-# you can also alter tables
-# if only you could access them
-migration "everything's better with bling" do
-  database.alter_table :foos do
-    drop_column :baz
-    add_column :bling, :float
-  end
-end
-
-puts "The foo table does exist!" if database.table_exists?('foos')
-=end
-
