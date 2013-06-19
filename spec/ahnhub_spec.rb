3da519d7f4574d8af47356418ac5f9975f6818fd
Justin Aiken <60tonangel@gmail.com>
2013-06-19 15:25:48 -0400

    Spec for RubyGems posting

--- spec/ahnhub_spec.rb
+++ spec/ahnhub_spec.rb
@@ -8,4 +8,12 @@ describe AhnHub do
       last_response.body.should match(/adhearsion/)
     end
   end
+
+  describe "POST '/rubygem_hook'" do
+    after { post '/rubygem_hook', RUBY_SAMPLE_RESPONSE.to_json, {'Content-Type' => 'application/json'} }
+
+    it "passes the info along to RubyGemUpdate" do
+      RubyGemUpdate.should_receive(:handle_hook)
+    end
+  end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,3 +18,22 @@ RSpec.configure do |config|
 end
 
 require File.dirname(__FILE__) + "/../ahnhub.rb"
+
+RUBY_SAMPLE_RESPONSE = {
+  "name"              => "testfoo123",
+  "downloads"         => 7,
+  "version"           => "0.0.2",
+  "version_downloads" => 0,
+  "platform"          => "ruby",
+  "authors"           => "Justin Aiken",
+  "info"              => "A test gem",
+  "project_uri"       => "http://rubygems.org/gems/testfoo123",
+  "gem_uri"           => "http://rubygems.org/gems/testfoo123-0.0.2.gem",
+  "homepage_uri"      => nil,
+  "wiki_uri"          => nil,
+  "documentation_uri" => nil,
+  "mailing_list_uri"  => nil,
+  "source_code_uri"   => nil,
+  "bug_tracker_uri"   => nil,
+  "dependencies"      => { "development" => [], "runtime" => []}
+}
