8655f734d7fc1e81aab123664d7d7681c3ca6ea1
Jamey Owens <jowens@mojolingo.com>
2013-06-28 19:44:30 -0400

    Load configs outside of modular app section

--- ahnhub.rb
+++ ahnhub.rb
@@ -1,35 +1,29 @@
 require 'json'
 require 'haml'
-require 'sinatra'
+require 'sinatra/base'
 require 'sinatra/sequel'
 require "sinatra/content_for"
 require "sinatra/reloader"
 require 'airbrake'
+require 'logger'
 
 require File.dirname(__FILE__) + "/lib/notifications.rb"
 require File.dirname(__FILE__) + "/lib/database.rb"
 
+Airbrake.configure do |config|
+  config.api_key    = ENV['ERRBIT_API_KEY']
+  config.host       = ENV['ERRBIT_API_HOST']
+  config.port       = 80
+  config.secure     = config.port == 443
+  config.logger     = Logger.new STDOUT
+end
+
 class AhnHub < Sinatra::Base
+  use Airbrake::Sinatra
+  enable :raise_errors
   helpers do
     include Sinatra::ContentFor
     include Rack::Utils
-    alias_method :h, :escape_html
-  end
-
-  configure :production do
-    set :logging, Logger::DEBUG
-
-    Airbrake.configure do |config|
-      config.api_key    = ENV['ERRBIT_API_KEY']
-      puts "API_KEY: #{config.api_key}"
-      config.host       = ENV['ERRBIT_API_HOST']
-      puts "API_HOST: #{config.host}"
-      config.port       = 80
-      config.secure     = config.port == 443
-    end
-
-    use Airbrake::Rack
-    enable :raise_errors
   end
 
   configure :development do
