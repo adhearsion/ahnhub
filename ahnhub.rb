398d7911d51776472a6afe6f073dfc12b8bf8fc6
Jamey Owens <jowens@mojolingo.com>
2013-06-28 20:50:45 -0400

    Add airbrake support for sinatra version of ahnhub

--- ahnhub.rb
+++ ahnhub.rb
@@ -1,15 +1,34 @@
 require 'json'
 require 'haml'
 require 'sinatra'
+require 'sinatra/base'
 require 'sinatra/sequel'
 require "sinatra/content_for"
 require "sinatra/reloader"
+require 'airbrake'
+require 'logger'
 
 require File.dirname(__FILE__) + "/lib/notifications.rb"
 require File.dirname(__FILE__) + "/lib/database.rb"
 
+
+Airbrake.configure do |config|
+  config.api_key    = ENV['ERRBIT_API_KEY']
+  config.host       = ENV['ERRBIT_API_HOST']
+  config.port       = 80
+  config.secure     = config.port == 443
+  config.logger     = Logger.new STDOUT
+  config.development_environments = []
+  config.notifier_version = '2.2'
+end
+
 class AhnHub < Sinatra::Base
-  helpers Sinatra::ContentFor
+  use Airbrake::Sinatra
+  enable :raise_errors
+  helpers do
+    include Sinatra::ContentFor
+    include Rack::Utils
+  end
 
   configure :development do
     register Sinatra::Reloader
@@ -32,3 +51,4 @@ class AhnHub < Sinatra::Base
 end
 
 AhnHub.run! if __FILE__ == $0
+
